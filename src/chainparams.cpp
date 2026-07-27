a444c722cfa17bf5d37caeed46b750550468a24c
Rux Crypto.ba <rux@crypto.ba>
2026-07-27 16:03:40 +0200

    Revert "Update chainparams.cpp"
    This reverts commit 97bd1625648fe3b38de1224b50542cc89cb0ff83.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -333,7 +333,7 @@ public:
         consensus.llmqs[Consensus::LLMQ_50_60] = llmq50_60;
         consensus.llmqs[Consensus::LLMQ_400_60] = llmq400_60;
         consensus.llmqs[Consensus::LLMQ_400_85] = llmq400_85;
-		consensus.llmqChainLocks = Consensus::LLMQ_50_60;
+        consensus.llmqChainLocks = Consensus::LLMQ_400_60;
         consensus.llmqForInstantSend = Consensus::LLMQ_50_60;
 
         fMiningRequiresPeers = true;
